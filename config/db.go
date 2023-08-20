b1826e37b2c2f3d53ba718fcc41134211367902d
RAJARSHI ROY <70441585+rijurajarshi@users.noreply.github.com>
2023-08-20 11:26:39 +0530

    Update db.go

--- config/db.go
+++ config/db.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func DBConnect() {
-	dsn := "root:#GIVE_YOUR_DB_PASSWORD@tcp(127.0.0.1:3306)/charging_station?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:GIVE_YOUR_DB_PASSWORD@tcp(127.0.0.1:3306)/charging_station?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
